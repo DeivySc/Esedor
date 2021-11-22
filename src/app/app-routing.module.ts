c7ed830c702e33d512b95a554230ee6ef308a5c0
Deivy Jampool Sánchez Coronado <80715089+DeivySc@users.noreply.github.com>
2021-11-22 09:26:55 -0500

    deploy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LandingComponent} from "./landing/landing.component";
 import {ContactComponent} from "./contact/contact.component";
+import {ProjectComponent} from "./project/project.component";
 
 const routes: Routes = [
   {path: '', redirectTo: 'landing', pathMatch: 'full'},
   {path: 'landing', component: LandingComponent},
-  {path: 'contacto', component: ContactComponent}
+  {path: 'contacto', component: ContactComponent},
+  {path: 'proyectos', component: ProjectComponent}
 ];
 
 @NgModule({
