2c951583923c67848d56cdd0c93ae8f677feaf54
Deivy Jampool Sánchez Coronado <80715089+DeivySc@users.noreply.github.com>
2021-11-22 14:59:54 -0500

    actualizaciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import {MatInputModule} from "@angular/material/input";
 import {MatIconModule} from "@angular/material/icon";
 import {MatSidenavModule} from "@angular/material/sidenav";
 import {MatListModule} from "@angular/material/list";
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import {MatListModule} from "@angular/material/list";
     LandingComponent,
     ContactComponent,
     ProjectComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
