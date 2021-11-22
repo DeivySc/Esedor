fd614fd4c18dd3428b88795cfb96f5a611fe23b1
Deivy Jampool Sánchez Coronado <80715089+DeivySc@users.noreply.github.com>
2021-11-22 10:54:07 -0500

    project view complete

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectComponent implements OnInit {
 
+  empresas = [
+    {name: 'Primax', descripcion: 'Pagos simples', img: 'assets/img/project/project01.png', alt: 'Primax'},
+    {name: 'Redeban', descripcion: 'Transformando el Ecommerce ', img: 'assets/img/project/project02.png', alt: 'Redeban'},
+    {name: 'Niubiz', descripcion: 'Transformando el Ecommerce ', img: 'assets/img/project/project03.png', alt: 'Niubiz'},
+    {name: 'Ripley', descripcion: 'Transformando el Ecommerce ', img: 'assets/img/project/project04.png', alt: 'Ripley'},
+    {name: 'Notifai', descripcion: 'Transformando el Ecommerce ', img: 'assets/img/project/project01.png', alt: 'Notifai'},
+    {name: 'Caja Metropolitana', descripcion: 'Transformando el Ecommerce ', img: 'assets/img/project/project02.png', alt: 'Caja Metropolitana'}
+  ]
+
   constructor() { }
 
   starproject_1 = true;
